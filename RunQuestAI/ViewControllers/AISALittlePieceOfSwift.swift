764d56927bee30daf0619756819e52bd4a0987b3
Rucisist <andreyilse@yandex.ru>
2018-03-04 13:58:38 +0300

    import swift

---
+++ RunQuestAI/ViewControllers/AISALittlePieceOfSwift.swift
@@ -0,0 +1,9 @@
+//
+//  AISALittlePieceOfSwift.swift
+//  RunQuestAI
+//
+//  Created by Андрей Илалов on 04.03.2018.
+//  Copyright © 2018 Андрей Илалов. All rights reserved.
+//
+
+import Foundation
