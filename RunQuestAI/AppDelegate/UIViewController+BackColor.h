d0ef4bd8b29d79d266083c86a2054fe9ede3b44e
Rucisist <andreyilse@yandex.ru>
2018-03-08 17:27:30 +0300

    changed color of tab and nav

---
+++ RunQuestAI/AppDelegate/UIViewController+BackColor.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+BackColor.h
+//  RunQuestAI
+//
+//  Created by Андрей Илалов on 08.03.2018.
+//  Copyright © 2018 Андрей Илалов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (BackColor)
+
+@end
